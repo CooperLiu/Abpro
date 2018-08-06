68e53ae9e3b9a87172b9ea41d8b6379a8f26d8c6
CooperLiu <liuyankan@jk724.com>
2018-08-06 18:50:03 +0800

    增加API审计日志处理

---
+++ src/Abpro.MessageBus/Publisher/ApiAuditing/ApiAuditingStore.cs
@@ -0,0 +1,14 @@
+﻿using System.Threading.Tasks;
+using Abp.Auditing;
+using Abp.Dependency;
+
+namespace Abpro.MessageBus.Publisher.ApiAuditing
+{
+    public class ApiAuditingStore : IAuditingStore
+    {
+        public Task SaveAsync(AuditInfo auditInfo)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
